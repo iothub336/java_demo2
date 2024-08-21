0cee9c38b507b6f398d14823a32d66ce34d66df9
iothub336 <iothub336@gmail.com>
2024-08-21 12:01:51 +0530

    new code 2

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for a username
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+
+        // Prompt the user for a password
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+
+        // Output the registration message
+        System.out.println("["+username + "] has been registered");
+
+        // Close the scanner
+        scanner.close();
+    }
+}
